702f3a1d13047f100246e3a162126421d65c114d
amacha2626 <amacha2626@gmail.com>
2020-05-27 19:55:58 +0900

    add theatrecompany routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'theatrecompanies/new'
   devise_for :users
   root to: 'top#index'
 
   resources :users, only: [:show, :edit, :update]
+  resources :theatrecompanies, only: [:new]
 end
