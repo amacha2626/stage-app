67df60ec8637fbb3977e2c68554a21ac86e85df0
amacha2626 <amacha2626@gmail.com>
2020-05-31 02:50:49 +0900

    add theatrecompany show routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root to: 'top#index'
 
   resources :users, only: [:show, :edit, :update]
-  resources :theatrecompanies, only: [:new, :create]
+  resources :theatrecompanies, only: [:show, :new, :create]
 end
