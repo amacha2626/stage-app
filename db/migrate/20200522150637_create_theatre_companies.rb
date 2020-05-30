961f014063f412e9a427bfd886c2ffd171c24b20
amacha2626 <amacha2626@gmail.com>
2020-05-31 02:49:56 +0900

    fix theatrecomapnies table

--- db/migrate/20200522150637_create_theatre_companies.rb
+++ db/migrate/20200522150637_create_theatre_companies.rb
@@ -3,12 +3,13 @@ class CreateTheatreCompanies < ActiveRecord::Migration[5.2]
     create_table :theatre_companies do |t|
       t.string :name, null: false
       t.string :name_kana, null: false
+      t.string :image
       t.integer :postal_code, null: false
       t.string :prefecture, null: false
       t.string :address, null: false
       t.string :website
       t.integer :establish
-      t.references :member, foreign_key: { to_table: :users}
+      t.references :member, foreign_key: { to_table: :users}, unique: true
       t.text :profile
       t.string :contact
       t.references :founder, foreign_key: { to_table: :users}, null: false
