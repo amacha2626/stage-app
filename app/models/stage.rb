af94f02ac05450f44b8fd3a2700024f7f89fe2a4
amacha2626 <amacha2626@gmail.com>
2020-06-04 02:39:28 +0900

    add validation

--- app/models/stage.rb
+++ app/models/stage.rb
@@ -1,2 +1,14 @@
 class Stage < ApplicationRecord
+  mount_uploader :image, ImageUploader
+
+  belongs_to :theatre_company
+
+  validates :title, presence: true
+  validates :theater, presence: true
+  validates :startperiod, presence: true
+  validates :lastperiod, presence: true
+  validates :lowestprice, presence: true
+  validates :highestprice, presence: true
+  validates :explanation, presence: true
+  validates :category, presence: true
 end
