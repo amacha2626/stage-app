a3773b219959ab31b66f35fd20c4305af9f8454c
amacha2626 <amacha2626@gmail.com>
2020-06-04 04:00:27 +0900

    add routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'reviews/show'
-  get 'reviews/new'
   devise_for :users
   root to: 'top#index'
 
   resources :users, only: [:show, :edit, :update]
   resources :theatrecompanies, only: [:show, :new, :create, :edit, :update]
-  resources :stages, only: [:show, :new, :create, :edit, :update]
+  resources :stages, only: [:show, :new, :create, :edit, :update] do
+    resources :reviews, only: [:new, :create]
+  end
 end
