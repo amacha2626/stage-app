3af226dab6cf8b1079b2ba965cf7f8705d795ddf
amacha2626 <amacha2626@gmail.com>
2020-06-04 04:01:36 +0900

    add review new action and page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,26 @@
 class ReviewsController < ApplicationController
-  def show
-  end
+  before_action :set_stage
 
   def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    if @review.save
+      redirect_to stage_path(@review.stage_id)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_stage
+    @stage = Stage.find(params[:stage_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:text, :score, :spoiler).merge(user_id: current_user.id, stage_id: params[:stage_id])
   end
 end
