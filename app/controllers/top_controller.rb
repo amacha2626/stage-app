049aa9ca97c1f891c5f2ee70b04690b99150f3ea
amacha2626 <amacha2626@gmail.com>
2020-06-04 02:39:48 +0900

    fix top page

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,7 @@
 class TopController < ApplicationController
   def index
+    @stages = Stage.all.limit(4)
+    @theatrecompanies = TheatreCompany.all.limit(6)
+    @users = User.all.limit(3)
   end
 end
